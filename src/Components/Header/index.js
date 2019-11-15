e7f7a7b5c23dcc0f531d97d0fd4688ff24e2ba16
Davi augusto <daviaugusto.developer@gmail.com>
2019-11-15 11:42:33 -0300

    Header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component{
+  render(){
+    return(
+      <div>
+        <strong>Filmaria</strong>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import Header from './Components/Header';
+
 import Home from './pages/Home';
 
 const Routes = () =>{
   return(
     <BrowserRouter>
+      <Header/>
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
