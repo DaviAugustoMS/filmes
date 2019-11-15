93a884cd8e184edae12639e0fdf3d730f696ad20
Davi augusto <daviaugusto.developer@gmail.com>
2019-11-15 12:05:36 -0300

    header ok

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import './header.css';
+import { Link } from 'react-router-dom';
 
 class Header extends Component{
   render(){
     return(
-      <div>
-        <strong>Filmaria</strong>
+      <div className="header"  >
+        <Link to='/'>Filmaria</Link>
       </div>
     );
   }
