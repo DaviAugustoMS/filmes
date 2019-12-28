031d1fbfda8e9cb41ddebb61b7fa866d3451b295
Davi augusto <daviaugusto.developer@gmail.com>
2019-12-28 13:04:36 -0300

    pagina erro

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,14 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from './Components/Header';
 
 import Home from './pages/Home';
-
+import Erro from './pages/Erro'
 const Routes = () =>{
   return(
     <BrowserRouter>
       <Header/>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="*" component={Erro} />
       </Switch>
     </BrowserRouter>
   );
